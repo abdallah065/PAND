2c1baf7e671788d4b2034be2312161e9181b2c93
Abdallah Saber <abdallahsaber065@gmail.com>
2023-02-11 20:48:55 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -159,7 +159,7 @@ else:
     #split the screen into 2 columns one for the detection method and the update interval for automatic detection
     col1, col2 = st.columns(2)
     with col1:
-        option = st.selectbox("اختر طريقة الكشف", ["كشف تلقائي", "الكشف اليدوي"])    
+        option = st.selectbox("اختر طريقة الكشف", ["كشف تلقائي", "كشف اليدوي"])    
     with col2:
         if option == "كشف تلقائي":
             time_update = st.number_input("فترة التحديث (بالثواني)", min_value=1, value=10, step=1, max_value=1000)
